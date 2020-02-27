7bdc4216140f786ddeb74a8ae3cce718d8faccb6
Abhishek-create824 <54523696+Abhishek-create824@users.noreply.github.com>
2020-02-27 15:31:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = 80;
+var port = 4000;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+console.log("Server running at http://34.209.79.39:%d", port);
